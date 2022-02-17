60a068beca46d15acdf497cfe54d3144984a99b6
OLIET2357 <50865315+OLIET2357@users.noreply.github.com>
2022-02-17 18:11:56 +0900

    basic feature

---
+++ app/src/main/java/com/example/qranalyzer/QRReaderActivity.kt
@@ -0,0 +1,37 @@
+package com.example.qranalyzer
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+
+
+class QRReaderActivity : AppCompatActivity() {
+    private val barcodeLauncher = registerForActivityResult(
+        ScanContract()
+    ) { result: ScanIntentResult ->
+        if (result.contents == null) {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(
+                this,
+                "Scanned: " + result.contents,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrreader)
+
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setOrientationLocked(false)
+        options.setBeepEnabled(false)
+
+        barcodeLauncher.launch(options)
+    }
+}

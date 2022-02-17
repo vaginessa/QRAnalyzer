f84fe4fb1f44240feb48c60339911c9a4a9260bc
OLIET2357 <50865315+OLIET2357@users.noreply.github.com>
2022-02-17 18:41:47 +0900

    move vibrate to result

--- app/src/main/java/com/example/qranalyzer/QRReaderActivity.kt
+++ app/src/main/java/com/example/qranalyzer/QRReaderActivity.kt
@@ -1,9 +1,6 @@
 package com.example.qranalyzer
 
-import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import android.os.Vibrator
 import androidx.appcompat.app.AppCompatActivity
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
@@ -18,8 +15,6 @@ class QRReaderActivity : AppCompatActivity() {
     private val barcodeLauncher = registerForActivityResult(
         ScanContract()
     ) { result: ScanIntentResult ->
-        vibrate(300)
-
         val resultMessage = "${getString(R.string.content)}: ${result.contents}\n\n" +
                 "${getString(R.string.hex)}: ${result.rawBytes.toHex()}"
 
@@ -40,10 +35,5 @@ class QRReaderActivity : AppCompatActivity() {
         options.setBeepEnabled(false)
 
         barcodeLauncher.launch(options)
-    }
-
-    private fun vibrate(milliseconds: Long) {
-        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        vibrator.vibrate(milliseconds)
     }
 }
--- app/src/main/java/com/example/qranalyzer/ResultActivity.kt
+++ app/src/main/java/com/example/qranalyzer/ResultActivity.kt
@@ -1,6 +1,8 @@
 package com.example.qranalyzer
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Vibrator
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,9 +16,15 @@ class ResultActivity : AppCompatActivity() {
         val textViewResult = findViewById<TextView>(R.id.textViewResult)
 
         if (result != null) {
+            vibrate(300)
             textViewResult.text = result
         } else {
             textViewResult.text = getString(R.string.message_null_result_error)
         }
+    }
+
+    private fun vibrate(milliseconds: Long) {
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        vibrator.vibrate(milliseconds)
     }
 }
